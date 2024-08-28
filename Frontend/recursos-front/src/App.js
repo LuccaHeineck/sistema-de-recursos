5b9679955ff27ede4704ea31d231b12a17830c15
Lucca <luccacheineck@gmail.com>
2024-08-28 20:01:46 -0300

    config django

--- Frontend/recursos-front/src/App.js
+++ Frontend/recursos-front/src/App.js
@@ -5,9 +5,6 @@ function App() {
   return (
     <div className="App">
       <HelloWorld />
-      <div className="flex justify-center items-center h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind!</h1>
-      </div>
     </div>
   );
 }
