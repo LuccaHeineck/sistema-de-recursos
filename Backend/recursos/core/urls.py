ab8aef57f549a9cc0a0730a765e70f215cf5310d
Lucca <luccacheineck@gmail.com>
2024-09-04 22:32:02 -0300

    serializer

--- Backend/recursos/core/urls.py
+++ Backend/recursos/core/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello-world/', views.hello_world, name='hello_world'),
-    
+    path('hello-world/', views.Hello_world.as_view()    , name='hello_world'),
 ]
--- Backend/recursos/core/views.py
+++ Backend/recursos/core/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
+class Hello_world(APIView):
+    def get(self, request):
+        return Response({'message': 'PIONMTO, world!'})
+    
+    def post(self, request):
+        # Supondo que os dados esperados sejam enviados no corpo da requisição (request.data)
+        data = request.data
+        
+        if 'name' in data:
+            message = f'Hello, {data["name"]}!'
+            return Response({'message': message}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'No name provided'}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
-def hello_world(request):
-    return Response({'message': 'PAIA, world!'})
