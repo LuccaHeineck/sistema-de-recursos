5d52fd83ba2bbed51b2d488d104ff21e200f1573
Lucca <luccacheineck@gmail.com>
2024-09-10 17:11:56 -0300

    Header ajustado

--- Frontend/recursos-front/src/components/Header/index.js
+++ Frontend/recursos-front/src/components/Header/index.js
@@ -1,7 +1,11 @@
+import { useLocation } from "react-router-dom";
+
 const Header = () => {
+  const location = useLocation();
+
   return (
-    <header className="bg-customGrey border-b-1 text-white p-4 h-16">
-      <h1>Header</h1>
+    <header className="bg-customGrey border-b-1 border-slate-400 text-white p-4 h-16">
+      <h1 className="text-xl font-bold">{location.pathname}</h1>
     </header>
   );
 };
