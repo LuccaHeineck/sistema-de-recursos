c07476dddb70d1ede8aafdc0a2d042f3a2eb7751
Lucca <luccacheineck@gmail.com>
2024-09-30 21:34:31 -0300

    Atualizando o login

--- Frontend/recursos-front/src/components/Layout/index.js
+++ Frontend/recursos-front/src/components/Layout/index.js
@@ -1,14 +1,14 @@
 import Menu from "../Menu";
 import Header from "../Header";
 
-const Layout = ({ children }) => {
+const Layout = ({ username, children }) => {
   return (
     <div className="bg-customGrey font-poppins flex h-screen">
       <Menu />
       <div className="flex-1 flex flex-col ml-72">
         {" "}
         {/* Adjust ml-60 based on Menu width */}
-        <Header />
+        <Header username={username} />
         <main className="flex-1 p-6 bg-customGrey text-white">{children}</main>
       </div>
     </div>
--- Frontend/recursos-front/src/components/LoginPage/index.js
+++ Frontend/recursos-front/src/components/LoginPage/index.js
@@ -1,51 +1,61 @@
+// src/components/LoginPage.js
+
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const LoginPage = ({ setIsAuthenticated }) => {
+const LoginPage = ({ setMessage, setIsAuthenticated }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:8000/api/token/", {
-        username,
-        password,
-      });
+      const response = await axios.post(
+        "http://127.0.0.1:8000/recursos/login/",
+        {
+          username,
+          password,
+        },
+        { withCredentials: true }
+      ); // Envia cookies de sessão
 
-      // Assuming your API returns the access token
-      const accessToken = response.data.access;
-      localStorage.setItem("accessToken", accessToken);
-
-      // Set authentication state to true
-      setIsAuthenticated(true);
-
-      // Optionally, redirect to a protected route after successful login
-      window.location.href = "/bem"; // Redirect to the home page or dashboard
+      setIsAuthenticated(true); // Define o estado de autenticação para true
+      localStorage.setItem("accessToken", "some-token"); // Simular token (opcional)
+      setError("");
+      setMessage(response.data.message);
+      navigate("/bem");
     } catch (error) {
-      console.error("Login failed:", error);
-      // Handle error (e.g., show error message)
+      // Verifica se error.response existe antes de acessar data
+      setError(
+        error.response && error.response.data.error
+          ? error.response.data.error
+          : "Erro ao fazer login"
+      );
     }
   };
 
   return (
     <div>
-      <h2>Login Page</h2>
-      <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="Username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
         />
         <input
           type="password"
-          placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
         />
         <button type="submit">Login</button>
       </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
 };
